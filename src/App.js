d1951499fd335d0be5cf40c491f80fe3a3d51450
Sayem92 <sayemmiha123@gmail.com>
2022-12-27 17:04:05 +0600

    add login form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { router } from './Routes/Routes/Routes';
@@ -8,6 +9,7 @@ function App() {
     <div>
      
       <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
      
     </div>
   );
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,4 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
+import Login from "../../component/Form/Login";
+import Register from "../../component/Form/Register";
 import Home from "../../component/Home/Home";
 import Main from "../../layout/Main";
 
@@ -10,6 +12,14 @@ export const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>,
+            },
+            {
+                path: '/login',
+                element: <Login></Login>,
+            },
+            {
+                path: '/register',
+                element: <Register></Register>,
             }
         ]
     }
