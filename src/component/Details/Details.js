0f21cdd0581efed9e21e58828e04560ea1beb80d
Sayem92 <sayemmiha123@gmail.com>
2022-12-29 12:53:01 +0600

    update detils

--- src/component/Details/Details.js
+++ src/component/Details/Details.js
@@ -6,7 +6,7 @@ const Details = () => {
 
     return (
         <div className='px-2 md:px-5 py-10 dark:bg-black'>
-            <div className="max-w-2xl overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+            <div className="max-w-2xl overflow-hidden rounded-lg shadow-lg dark:bg-gray-800 bg-gray-800">
                 <img className="object-cover w-full h-64 rounded-lg" src={image} alt="Article" />
 
                 <div className="p-6">
@@ -21,8 +21,8 @@ const Details = () => {
                                         className='px-2 py-3 rounded bg-red-500 text-white'>Not Completed</button>
                             }
                         </span>
-                        <h1 className="block mt-2 text-2xl font-semibold text-gray-800 transition-colors duration-300 transform dark:text-white" tabIndex="0" role="link">{title}</h1>
-                        <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">{description}</p>
+                        <h1 className="block mt-2 text-2xl font-semibold transition-colors duration-300 transform text-white" tabIndex="0" role="link">{title}</h1>
+                        <p className="mt-2 text-sm text-gray-200">{description}</p>
                     </div>
                 </div>
             </div>
