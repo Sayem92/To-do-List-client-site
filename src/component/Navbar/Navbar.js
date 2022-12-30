e20e0fadc560436f44ee7bb278492d68971475c1
Sayem92 <sayemmiha123@gmail.com>
2022-12-30 11:25:32 +0600

    add responsive mobile

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -154,7 +154,7 @@ const Navbar = () => {
                     </button>
                     {isMenuOpen && (
                         <div className="z-10 absolute top-0 left-0 w-full">
-                            <div className="p-5 bg-white border rounded shadow-sm">
+                            <div className="p-5 bg-gray-900 border rounded shadow-sm">
                                 <div className="flex items-center justify-between mb-4">
                                     <div>
                                         <Link
@@ -178,7 +178,7 @@ const Navbar = () => {
                                         >
                                             <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
                                                 <path
-                                                    fill="currentColor"
+                                                    fill="#ffffff"
                                                     d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
                                                 />
                                             </svg>
@@ -188,34 +188,43 @@ const Navbar = () => {
                                 <nav>
                                     <ul className="space-y-4">
                                         <li>
-                                            <Link
+                                            <NavLink
+                                            style={({ isActive }) =>
+                                            isActive ? activeStyle : undefined
+                                          }
                                                 to="/addTask"
                                                 aria-label="Add Task"
                                                 title="Add Task"
-                                                className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                                                className="font-medium tracking-wide text-gray-200 transition-colors duration-200 hover:text-deep-purple-accent-400"
                                             >
                                                 Add Task
-                                            </Link>
+                                            </NavLink>
                                         </li>
                                         <li>
-                                            <Link
+                                            <NavLink
+                                            style={({ isActive }) =>
+                                            isActive ? activeStyle : undefined
+                                          }
                                                 to="/myTask"
                                                 aria-label="My Task"
                                                 title="My Task"
-                                                className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                                                className="font-medium tracking-wide text-gray-200 transition-colors duration-200 hover:text-deep-purple-accent-400"
                                             >
                                                 My Task
-                                            </Link>
+                                            </NavLink>
                                         </li>
                                         <li>
-                                            <Link
+                                            <NavLink
+                                            style={({ isActive }) =>
+                                            isActive ? activeStyle : undefined
+                                          }
                                                 to="/completedTask"
                                                 aria-label="Completed Task"
                                                 title="Completed Task"
-                                                className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                                                className="font-medium tracking-wide text-gray-200 transition-colors duration-200 hover:text-deep-purple-accent-400"
                                             >
                                                 Completed Task
-                                            </Link>
+                                            </NavLink>
                                         </li>
                                         <li>
                                             {
